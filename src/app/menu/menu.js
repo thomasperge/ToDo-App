fdc78447bbe9fcefbbe927dc3450c005fd5a66c5
Thomas Kauffmant <thomaskauffmant74@gmail.com>
2022-10-06 19:26:52 +0200

    navBar version 1 finish

--- src/app/menu/menu.js
+++ src/app/menu/menu.js
@@ -1,17 +1,21 @@
-/**
- * texte here
- * @function
- */
-
 const MINUS = document.getElementById("minimize")
 const CLOSE_APP = document.getElementById("close-app")
 
 MINUS.addEventListener("click", minimize)
 CLOSE_APP.addEventListener("click", close_app)
 
+/**
+ * Close App
+ * @function
+ */
 function close_app(){
     app.window.close()
 }
+
+/**
+ * Minimalize App
+ * @function
+ */
 function minimize(){
     app.window.minimize()
 }

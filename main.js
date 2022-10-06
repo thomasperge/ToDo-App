c60f22abba8159beabb0629c1994cfe8b103ce05
Thomas Kauffmant <thomaskauffmant74@gmail.com>
2022-10-06 10:42:37 +0200

    windows test

--- main.js
+++ main.js
@@ -5,8 +5,9 @@ const path = require('path')
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1080,
+    height: 720,
+    autoHideMenuBar: true,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }

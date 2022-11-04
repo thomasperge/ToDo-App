fa3e920221975ee5014a0fcdfcd9c5fe6fe37439
Thomasperge <thomaskauffmant74@gmail.com>
2022-11-04 15:57:53 +0100

    fix: add second windows fix bug

--- main.js
+++ main.js
@@ -10,9 +10,10 @@ app.disableHardwareAcceleration();
 
 // Create the main windows
 let window;
+let window2
 
 /**
- * Main Function
+ * Main Function - Index Page (index.html)
  * @function
  */
 async function main() {
@@ -36,6 +37,7 @@ async function main() {
   window.loadFile('index.html')
 };
 
+
 /**
  * Close Main App Page
  */
@@ -51,46 +53,67 @@ ipcMain.on("appMain/minimize", () => {
 });
 
 /**
- * Add Task
+ * Close Add-Task Page
  */
-ipcMain.on("appMain/addTask2", () => {
-  function addTask(task) {    
-    return new Promise((resolve, reject) => {
-      data.task.allTask.push(task)
-
-      fs.writeFile('data.json', JSON.stringify(data), (err) => {
-        if (err) reject(err)
-        resolve("File saved.")
-      })
-    });
-  }
-
-  addTask('thomas1')
-  console.log(data.task.allTask)
-  window.reload()
+ ipcMain.on("appAdd/close", () => {
+  window2.close()
 });
+
+/**
+ * Minimize Add-Task Page
+ */
+ipcMain.on("appAdd/minimize", () => {
+  window2.minimize();
+});
+
+
+
+/**
+ * Add Task when button click
+ */
+// ipcMain.on("appMain/addTask2", () => {
+//   function addTask(task) {    
+//     return new Promise((resolve, reject) => {
+//       data.task.allTask.push(task)
+
+//       fs.writeFile('data.json', JSON.stringify(data), (err) => {
+//         if (err) reject(err)
+//         resolve("File saved.")
+//       })
+//     });
+//   }
+
+//   addTask('thomas1')
+//   console.log(data.task.allTask)
+//   window.reload()
+// });
 
 
 /**
  * When button Click, create new windows to add task
  */
 ipcMain.on("appMain/addTask", () => {
-  // create the browser window
-  let window2
+  const isPlayerWindowOpened = () => !window2?.isDestroyed() && window2?.isFocusable();
 
-  window2 = new BrowserWindow({
-    frame: false,
-    // autoHideMenuBar: true,
-    width: 640,
-    height: 420,
-    // resizable: false,
-    webPreferences: {
-      contextIsolation: true,
-      nodeIntegration: true,
-      preload: path.join(__dirname, "./src/preload.js") // use a preload script
-    }
-  })
+  // Check if windows2 is already open
+  if (!isPlayerWindowOpened()) {
+    // create the browser window
+    window2 = new BrowserWindow({
+      frame: false,
+      // autoHideMenuBar: true,
+      width: 640,
+      height: 420,
+      // resizable: false,
+      webPreferences: {
+        contextIsolation: true,
+        nodeIntegration: true,
+        preload: path.join(__dirname, "./src/preload.js") // use a preload script
+      }
+    });
 
-  window2.on("ready-to-show", window2.show)
-  window2.loadFile('addTask.html')
+    window2.on("ready-to-show", window2.show);
+    window2.loadFile('addTask.html');
+  } else {
+    console.log("can't open a new windows")
+  }
 });
--- src/preload.js
+++ src/preload.js
@@ -6,13 +6,28 @@ const API = {
     window: {
         close: () => ipcRenderer.send("appMain/close"),
         minimize: () => ipcRenderer.send("appMain/minimize")
-    }
+    },
 }
 
+// == Create New Window ==
 document.addEventListener('DOMContentLoaded', function() {
     let addButton = document.getElementById("addTask")
     addButton.addEventListener("click", () => {
         ipcRenderer.send("appMain/addTask")
+    })
+})
+
+// == Close and Minimize Windows2 ==
+document.addEventListener('DOMContentLoaded', function() {
+    let closeButtonAdd = document.getElementById("close-appAdd")
+    let minButtonAdd = document.getElementById("minimizeAdd")
+
+    closeButtonAdd.addEventListener("click", () => {
+        ipcRenderer.send("appAdd/close")
+    })
+
+    minButtonAdd.addEventListener("click", () => {
+        ipcRenderer.send("appAdd/minimize")
     })
 })
 

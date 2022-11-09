377187e3c5c5ab944200d83eb2823dae675df705
Thomasperge <thomaskauffmant74@gmail.com>
2022-11-09 23:15:33 +0100

    feat: add the fulle 3d effect on login page ^^

---
+++ src/mouse-effect.js
@@ -0,0 +1,12 @@
+const icon = document.getElementById("test")
+
+// == Test Cursor Effect ==
+document.onmousemove = function() {
+
+    let x = event.clientX * 5 / window.innerWidth/2 + "%";
+    let y = event.clientY * 10 / window.innerHeight/2 + "%";
+
+    icon.style.left = x;
+    icon.style.right = y;
+    icon.style.transform = "translate(-" + x + ",-" + y + ")";
+}
